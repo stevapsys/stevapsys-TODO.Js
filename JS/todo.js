2631c5278a7c46d555801c3cdccc4101effd9b9e
stevapsys <stephanievapsys@gmail.com>
2020-03-30 11:42:37 -0300

    criando a função adicionarTarefa, criando o evento enter e personlizando o estilo

--- JS/todo.js
+++ JS/todo.js
@@ -1,49 +1,46 @@
 let botao = document.getElementById('click');
-let container = document.querySelector ('.container');
+let container = document.querySelector ('main.container');
 let inputTextoTarefa = document.querySelector('#textoTarefa');
+let body = document.querySelector('body')
 
 let finalizar = document.getElementById('finalizar');
-let modal = document.querySelector(".card"); 
 
 
-finalizar.onclick = function(){
-
-    if(modal.getAttribute('class') == "show") {    
+function adicionarTarefa() {
+    let texto = inputTextoTarefa.value; 
+    if(texto.length > 0) {  
+        let card = document.createElement ('div')
+        card.setAttribute('class', 'card m-3')
+            
+        let cardBody = document.createElement('div')
+        cardBody.setAttribute('class', 'card-body')
+            
+        let btnFinalizarTarefa = document.createElement('button')
+        btnFinalizarTarefa.setAttribute('class', "btn btn-info")
+        btnFinalizarTarefa.textContent = "Finalizar tarefa";
+        btnFinalizarTarefa.onclick = function(event){
+                card.remove()
+            }
+            
+        card.appendChild(cardBody)
+        cardBody.textContent = texto;
+        cardBody.appendChild(btnFinalizarTarefa)
+    
+        container.appendChild(card)
+            
+        inputTextoTarefa.value = ""; 
+    } else {
+        alert("Você deve digitar uma tarefa primeiro")
     }
-    modal.setAttribute('class', 'modal hide');
 }
 
 
-botao.onclick = function() {
-    alert("Nova tarefa adicionada com sucesso!")
+botao.onclick = adicionarTarefa
 
-    let card = document.createElement ('div')
-    card.setAttribute('class', 'card')
-
-    let cardBody = document.createElement('div')
-    cardBody.setAttribute('class', 'card')
-
-    let btnFinalizarTarefa = document.createElement('button')
-    btnFinalizarTarefa.setAttribute('class', "btn btn-info")
-    btnFinalizarTarefa.textContent = "Finalizar tarefa";
-
-    let texto = inputTextoTarefa.value
-
-    card.appendChild(cardBody)
-    cardBody.textContent = texto;
-
-    cardBody.appendChild(btnFinalizarTarefa)
-
-    container.appendChild(card)
-
-    inputTextoTarefa.value = ""; 
-
-    btnFinalizarTarefa.onclick = function(){
-
-        if(card.getAttribute('class') == "show") {    
-        }
-        card.setAttribute('class', 'modal hide');
-    }
-
+//para adicionar tarefa com enter 
+inputTextoTarefa.onkeyup = function(event){
+    if(event.key == 'Enter') { 
+        adicionarTarefa()
+}
 }
 
